88f179a5fcaf8bf857b2948a64591264e8a6279d
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-25 09:21:10 -0600

    consumo de API con axios listo

--- src/views/store/menCategory.jsx
+++ src/views/store/menCategory.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Card } from "../../components/card/Card";
 import { SectionContainer } from "../../components/sectionContainer/SectionContainer";
 import { GridLayout } from "../../components/gridLayout/GridLayout";
 
-import { menCategory } from "../../components/card/cardData";
+//import { menCategory } from "../../components/card/cardData";
 export const MenCategory = () => {
+  const [card, setCard] = useState([]);
+  useEffect(() => {
+    const URL_ROOT =
+      "https://server-e-commerce-api.herokuapp.com/api/menproducts";
+    axios
+      .get(URL_ROOT)
+      .then((response) => {
+        const data = response.data;
+        setCard(data);
+      })
+      .catch((error) => error);
+  }, []);
   return (
     <SectionContainer title="Hombre">
       <GridLayout>
-        {menCategory.map((image) => (
+        {card.map((image) => (
           <Card
             img={image.url}
             title={image.title}
--- src/views/store/womanCategory.jsx
+++ src/views/store/womanCategory.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { SectionContainer } from "../../components/sectionContainer/SectionContainer";
 import { GridLayout } from "../../components/gridLayout/GridLayout";
 import { Card } from "../../components/card/Card";
 
-import { womanCategory } from "../../components/card/cardData";
-
 export const WomanCategory = () => {
+  const [card, setCard] = useState([]);
+  useEffect(() => {
+    const URL_ROOT =
+      "https://server-e-commerce-api.herokuapp.com/api/womanproducts";
+    axios
+      .get(URL_ROOT)
+      .then((response) => {
+        const data = response.data;
+        setCard(data);
+      })
+      .catch((error) => error);
+  }, []);
   return (
     <SectionContainer title="Mujer">
       <GridLayout>
-        {womanCategory.map((image) => (
+        {card.map((image) => (
           <Card
             img={image.url}
             title={image.title}

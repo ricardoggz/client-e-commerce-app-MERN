65f36c35ad0705444c29c596b793306571b13a70
ricardoggz <ricardo_ggzicm@outlook.com>
2022-01-28 15:40:18 -0600

    nav bar modificado para recibir el token de usuario

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { Button } from "../button/Button";
 import { RoutesApp } from "../../routes/RoutesApp";
 import { NavWrapper, NavItems, NavTitles, Navigation } from "./elements";
 import { BsCart3 } from "react-icons/bs";
+import { userContext } from "../../context/userContext";
 
 
 export const NavBar = () => {
+  const { user: token = null } = useContext(userContext);
   return (
     <Router>
     <NavWrapper>
       <NavTitles className="flex-container">
         <span className="logo"><Link to="/">STORE APP</Link></span>
-        <Link to="/login"><Button title="Acceder" /></Link>
+        {
+          token ? (
+            <Link to="/"><Button title="Salir" /></Link>
+          )
+          : (
+            <Link to="/login"><Button title="Acceder" /></Link>
+          )
+        }
       </NavTitles>
       <Navigation>
         <NavItems className="flex-container">
